94d950dc07a6f7fc2043772eb3b23714a75448d3
Njegos Petrovic <njegos.petrovic@profitoptics.com>
2024-08-24 11:15:45 +0200

    Add chat component

---
+++ src/Chat.tsx
@@ -0,0 +1,13 @@
+import { Contacts } from './components/contacts/Contacts';
+import { Conversation } from './components/conversation/Conversation';
+
+const Chat = () => {
+  return (
+    <>
+      <Contacts />
+      <Conversation />
+    </>
+  );
+};
+
+export default Chat;
