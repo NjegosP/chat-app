fb4ef2c7e056b766ebc140b7cab329c72351b04b
Njegos Petrovic <njegos.petrovic@profitoptics.com>
2024-08-25 20:10:29 +0200

    Remove unnecessary files and imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import './App.css';
 import Chat from './Chat';
 import { ConversationContextProvider } from './contexts/ConversationContext';
 
