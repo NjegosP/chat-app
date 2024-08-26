bfad326e17d091ce3406421b9d8fd19398be7c6f
Njegos Petrovic <njegos.petrovic@profitoptics.com>
2024-08-26 20:46:47 +0200

    Update contact and last message components

--- src/components/contacts/ContactItem/LastMessage.tsx
+++ src/components/contacts/ContactItem/LastMessage.tsx
@@ -2,7 +2,6 @@ export const LastMessage = ({ name, lastMessage }: { name: string; lastMessage: 
   <div className="flex-col px-3 overflow-hidden hidden sm:flex sm:flex-1">
     <div className="flex flex-row justify-between w-full">
       <div className="text-sm font-bold text-sky-500">{name}</div>
-      <div className="text-sm text-sky-400">Yesterday</div>
     </div>
     <div className="truncate flex-1 text-sky-400 font-medium">{lastMessage}</div>
   </div>
--- src/components/contacts/Contacts.tsx
+++ src/components/contacts/Contacts.tsx
@@ -7,15 +7,13 @@ export const Contacts = () => {
   if (isLoading) return <p>Loading ...</p>;
 
   return (
-    <div className="flex-1 w-auto p-3 bg-[#0F172A] sm:min-w-96 ">
-      <div className="sticky top-0">
-        {contacts.map((contact) => (
-          <ContactItem
-            key={contact.userId}
-            contact={contact}
-          />
-        ))}
-      </div>
+    <div className="flex-1 w-auto p-3 bg-[#0F172A] sm:min-w-96 h-screen sticky top-0 overflow-auto">
+      {contacts.map((contact) => (
+        <ContactItem
+          key={contact.userId}
+          contact={contact}
+        />
+      ))}
     </div>
   );
 };
